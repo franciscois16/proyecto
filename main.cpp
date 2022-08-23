dd8b95878102c5533a399c8b773c0d8a141ae8b3
franciscois16 <fmirchav@umag.cl>
2022-08-23 14:14:27 -0300

    sonido, niveles, puntuación y vidas

--- main.cpp
+++ main.cpp
@@ -16,11 +16,7 @@ BITMAP *disparobmp;
 BITMAP *dispa;
 BITMAP *trampabmp;
 
-int fil,col,i,j,z;	
-int direccion=0;
-int nivel=0;
-int impacto=0;
-int vidas=5;
+
 
  
  
@@ -34,41 +30,7 @@ void pantalla();
 void dibujarbala();
 void dibujarburbuja();
 void general();
-//char leido[600];
 
-/*
-char mapa[maxfilas][maxcolumnas]{
-"xxxxxxxxxxxxxxxxxxx",
-"xxxx              x",
-"xxx               x",
-"xx                x",
-"x                 x",		
-"x                 x",
-"x                 x",
-"x                 x",
-"x                 x",	
-"x                 x",
-"x                 x",		
-"x       x         x",
-"x       x         x",
-"xx      x         x",
-"xxx     x         x",
-"xx      x         x",
-"x       x         x",
-"x       x         x",
-"x                 x",
-"x                 x",
-"x                 x",		
-"x                 x",		
-"x                 x",		
-"x                 x",		
-"x                 x",							
-"x                 x",		
-"xx                x",
-"xxx               x",
-"xxxx              x",
-"xxxxxxxxxxxxxxxxxxx",};
-*/
 typedef struct{
 
 int direccion;
@@ -94,87 +56,6 @@ burbujas  BU[5];
 
 	
 	
-void cargarmapaarchivo(){
-
-if(nivel==0){
-
-FILE * mapa = fopen ("mapa.txt" , "r");
-if(mapa==NULL){
-	printf("error al abrir el archivo");
-	return;
-	
-}
-
-i=0;
-j=0;
-z=0;
-
-while(!feof (mapa) ){
- 		 
-		 leido = fgetc(mapa);
-		 if(leido=='b'){
-		 BU[z].posx=i*30;
-		 BU[z].posy=j*30;
-		 z++;
-		 printf("el valor de z es :%d",z);
-		  }
-		 
-		 if(leido=='\n'){
-		 i++;
-		 j=0;
-		  }
-		else{
-			mapa1[i][j]=leido;
-			j++;
-		}
-		
-		
-	
- }
- }
- 
- if(nivel==1){
-
-FILE * mapa = fopen ("mapa2.txt" , "r");
-if(mapa==NULL){
-	printf("error al abrir el archivo");
-	return;
-	
-}
-
-i=0;
-j=0;
-z=0;
-
-while(!feof (mapa) ){
- 		 
-		 leido = fgetc(mapa);
-		 if(leido=='b'){
-		 BU[z].posx=i*30;
-		 BU[z].posy=j*30;
-		 z++;
-		 printf("el valor de z es :%d",z);
-		  }
-		 
-		 if(leido=='\n'){
-		 i++;
-		 j=0;
-		  }
-		else{
-			mapa1[i][j]=leido;
-			j++;
-		}
-		
-		
-	
- }
- }
- 
- 
-	fclose;
-}
-	
-
 
 main()
 {
@@ -190,8 +71,19 @@ main()
 		return 1;
 	}
 	set_volume(70,70);
+	
+	int fil,col,i,j,z;	
+	int direccion=0;
+	int nivel=0;
+	int impacto=0;
+	int vidas=5;
+	int opcion=0;
+	int puntaje = 0;
+	void cargarmapaarchivo();
 		
 	MIDI *musica = load_midi("IMG/musicafondo.mid");
+	MIDI *musica2 = load_midi("IMG/river.mid");
+	MIDI *musicamenu = load_midi("IMG/musicamenu.mid");
 	SAMPLE *shot = load_wav("IMG/diparo.wav");
 	buffer = create_bitmap(900,570);
 	ladrillo = load_bitmap("IMG/ladrillo.bmp",NULL);
@@ -204,18 +96,30 @@ main()
 	burbuja = create_bitmap(30,30);
 	burbujabmp= load_bitmap("IMG/burbuja.bmp",NULL);
 	trampabmp= load_bitmap("IMG/trampa.bmp",NULL);
+	BITMAP  *menu =  load_bitmap("IMG/menu.bmp",NULL);
 	
-	 cargarmapaarchivo();
+	 
 BA.posx=50*25;
 BA.posy=51*10;
-//BU.direccion=1;
-//BU.posx=30*10;
-//BU.posy=30*10;
+while(opcion!=3){
+
+opcion = 0;
+vidas = 5;
+puntaje = 0;
+play_midi(musicamenu,1);
+while(opcion==0){
+	blit(menu,buffer,0,0,0,0,900,570);
+	blit(buffer,screen,0,0,0,0,900,570);
+	if(key[KEY_1]) 
+	opcion = 1;
+	if(key[KEY_3]) 
+	opcion = 3;
+}
  
- 
+ cargarmapaarchivo();
  play_midi(musica,1);
 
-	while(!key[KEY_ESC]&&vidas!=0&&nivel==0){
+	while(!key[KEY_ESC]&&vidas!=0&&nivel==0&&opcion==1){
 		
 	general();
 }
@@ -228,12 +132,28 @@ BA.posy=51*10;
 //BU.posy=30*10;
 
  impacto=0;
+play_midi(musica2,1);
 
-
-	while(!key[KEY_ESC]&&vidas!=0&&nivel==1){
+	while(!key[KEY_ESC]&&vidas!=0&&nivel==1&&opcion==1){
 		general();
 }
 
+ cargarmapaarchivo();
+BA.posx=50*25;
+BA.posy=51*10;
+//BU.direccion=1;
+//BU.posx=30*10;
+//BU.posy=30*10;
+
+ impacto=0;
+play_midi(musica2,1);
+
+	while(!key[KEY_ESC]&&vidas!=0&&nivel==2&&opcion==1){
+		general();
+}
+
+
+}
 	allegro_exit();
 	
 	return 0;
@@ -243,7 +163,7 @@ END_OF_MAIN();
 
 
 void general(){
-			if(key[KEY_RIGHT]) direccion = 0;
+			if(key[KEY_RIGHT]) direccion** = 0;
 				else if(key[KEY_LEFT]) direccion = 1;
 		 		else if(key[KEY_SPACE]){
 		 		
@@ -261,7 +181,7 @@ void general(){
 			  direccion = 6;
 			  else JJ.posx -= 30  ;
 			  }
-			  printf(" pos de x %d",JJ.posx);
+			  
 			  
 			if(direccion==0&&key[KEY_RIGHT]){
 			if(mapa1[(JJ.posx/30)+1][JJ.posy/30] == 'x')
@@ -284,7 +204,7 @@ void general(){
 			  if(mapa1[JJ.posx/30][JJ.posy/30]=='o'&& mapa1[JJ.posx/30][(JJ.posy/30)+1]=='o'){
 			  JJ.posy = JJ.posy+30;
 			  }
-			  printf(" pos de x %d",JJ.posx);
+	
 			 
 			if(BA.posy+30!='x'){
 				BA.posy+30;
@@ -293,7 +213,9 @@ void general(){
 			}
 		for(z=0;z<5;z++){
 			if(BA.posy/30 == BU[z].posy/30 && BA.posx/30 == BU[z].posx/30){
-		
+				
+				puntaje = puntaje + 100 * vidas;
+				printf(" puntaje %d",puntaje);
 				BA.posx=30*30;
 				BU[z].posy=30*30;
 				impacto++;
@@ -301,8 +223,13 @@ void general(){
 				nivel+=1;	
 				}
 				}
-			}	
-			 if(mapa1[JJ.posx/30][(JJ.posy/30)]=='t'&&key[KEY_RIGHT]){
+			}
+			if(mapa1[JJ.posx/30][(JJ.posy/30)+1]=='t'){
+			  JJ.posx = JJ.posx-60;
+			  vidas=vidas-1;
+			  }	
+				
+			if(mapa1[JJ.posx/30][(JJ.posy/30)]=='t'&&key[KEY_RIGHT]){
 			  JJ.posx = JJ.posx-60;
 			  vidas=vidas-1;
 			  }	
@@ -326,8 +253,12 @@ void dibujamapa(){
 	blit(bufbmp,buffer,0,0,0,0,30,32);
 	draw_sprite(buffer,bufbmp,0,0);
 	
+
+	
 	for(i=0;i<maxfilas;i++){
 		for(j=0;j<maxcolumnas;j++){
+			
+	//	textout(screen,font,"hola mundo",100,100,-1);
 		if (mapa1[i][j] == 'x'){
 				draw_sprite(buffer,ladrillo,i*30,j*30);
 				}
@@ -340,7 +271,8 @@ void dibujamapa(){
 				draw_sprite(buffer,trampabmp,i*30,j*30);
 							}	
 		}
-		
+		textprintf_ex(buffer,font,10,10,0xff0000,0x808080,"VIDAS : %d", vidas);
+		textprintf_ex(buffer,font,10,30,0xff0000,0x808080,"PUNTAJE : %d", puntaje);
 	}
 }
 
@@ -387,3 +319,128 @@ for(z=0;z<5;z++){
 	draw_sprite(buffer,burbuja,BU[z].posx,BU[z].posy);
 	}
 }
+
+
+void cargarmapaarchivo(){
+ 
+
+if(nivel==0)
+{
+
+FILE * mapa = fopen ("mapa.txt" , "r");
+if(mapa==NULL){
+	printf("error al abrir el archivo");
+	return;
+	
+}
+
+i=0;
+j=0;
+z=0;
+
+while(!feof (mapa) ){
+ 		 
+		 leido = fgetc(mapa);
+		 if(leido=='b'){
+		 BU[z].posx=i*30;
+		 BU[z].posy=j*30;
+		 z++;
+		 
+		  }
+		 
+		 if(leido=='\n'){
+		 i++;
+		 j=0;
+		  				}
+		else{
+			mapa1[i][j]=leido;
+			j++;
+			}
+		
+		
+	
+ 					}
+ }
+ 
+ if(nivel==1){
+
+FILE * mapa = fopen ("mapa2.txt" , "r");
+if(mapa==NULL){
+	printf("error al abrir el archivo");
+	return;
+	
+			}
+
+i=0;
+j=0;
+z=0;
+
+while(!feof (mapa) ){
+ 		 
+		 leido = fgetc(mapa);
+		 if(leido=='b')	{
+		 BU[z].posx=i*30;
+		 BU[z].posy=j*30;
+		 z++;
+		
+		  				}
+		 
+		 if(leido=='\n'){
+		 i++;
+		 j=0;
+		  				}
+		else{
+			mapa1[i][j]=leido;
+			j++;
+						}
+		
+		
+	
+ 					}
+ 
+ 
+ 
+ 			}
+ 
+ if(nivel==2){
+
+FILE * mapa = fopen ("mapa3.txt" , "r");
+if(mapa==NULL){
+	printf("error al abrir el archivo");
+	return;
+	
+}
+
+i=0;
+j=0;
+z=0;
+
+while(!feof (mapa) ){
+ 		 
+		 leido = fgetc(mapa);
+		 if(leido=='b'){
+		 BU[z].posx=i*30;
+		 BU[z].posy=j*30;
+		 z++;
+		
+		  }
+		 
+		 if(leido=='\n'){
+		 i++;
+		 j=0;
+		  }
+		else{
+			mapa1[i][j]=leido;
+			j++;
+		}
+		
+		
+	
+ 					}
+ 
+ 			} 
+	fclose;
+}
+	
+
+
